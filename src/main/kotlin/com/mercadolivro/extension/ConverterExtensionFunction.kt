38cc6915557f1ff1880597008cd9e639ad53348a
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-07-22 02:15:25 -0300

    Created post book request

--- src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -1,7 +1,9 @@
 package com.mercadolivro.extension
 
+import com.mercadolivro.controller.request.*
+import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
-import com.mercadolivro.request.*
 
 fun PostCustomerRequest.toCostumerModel(): CustomerModel {
     return CustomerModel(name = this.name, email = this.email)
@@ -13,4 +15,23 @@ fun PutCustomerRequest.toCostumerModel(id: Int): CustomerModel {
 
 fun GetCustomerRequest.toCostumerModel(id: Int): CustomerModel {
     return CustomerModel(id = id, name = this.name, email = this.email);
+}
+
+fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel {
+    return BookModel(
+        name = this.name,
+        price = this.price,
+        status = BookStatus.ATIVO,
+        customer =  customer
+    )
+}
+
+fun PutBookRequest.toBookModel(previousValues: BookModel): BookModel {
+    return BookModel(
+        id = previousValues.id,
+        name = this.name ?: previousValues.name,
+        price = this.price ?: previousValues.price,
+        status = previousValues.status,
+        customer = previousValues.customer
+    )
 }
