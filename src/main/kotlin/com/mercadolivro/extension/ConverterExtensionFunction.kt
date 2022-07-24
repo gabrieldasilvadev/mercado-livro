ae4924aca854c8133657a88b5ed13e472da7f629
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-07-24 17:03:45 -0300

    Updated PutCustumerRequest, GetCustomerRequest and PostCustomerRequest

--- src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -2,19 +2,20 @@ package com.mercadolivro.extension
 
 import com.mercadolivro.controller.request.*
 import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 
 fun PostCustomerRequest.toCostumerModel(): CustomerModel {
-    return CustomerModel(name = this.name, email = this.email)
+    return CustomerModel(name = this.name, email = this.email, status = CustomerStatus.ATIVO)
 }
 
-fun PutCustomerRequest.toCostumerModel(id: Int): CustomerModel {
-    return CustomerModel(id = id, name = this.name, email = this.email)
+fun PutCustomerRequest.toCostumerModel(previousValues: CustomerModel): CustomerModel {
+    return CustomerModel(id = previousValues.id, name = this.name, email = this.email, status = previousValues.status);
 }
 
 fun GetCustomerRequest.toCostumerModel(id: Int): CustomerModel {
-    return CustomerModel(id = id, name = this.name, email = this.email);
+    return CustomerModel(id = id, name = this.name, email = this.email, this.status);
 }
 
 fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel {
