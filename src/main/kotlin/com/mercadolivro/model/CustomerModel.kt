9bba328e5bc3b4e65e8e46558bffe9e99b3b550b
Gabriel da Silva <gabrieldasilvadev@gmail.com>
2022-07-24 17:06:40 -0300

    Added enum status on customer model

--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,10 +1,7 @@
 package com.mercadolivro.model
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import com.mercadolivro.enums.CustomerStatus
+import javax.persistence.*
 
 @Entity(name = "customer")
 data class CustomerModel(
@@ -17,4 +14,8 @@ data class CustomerModel(
 
   @Column()
   var email: String,
+
+  @Column()
+  @Enumerated(EnumType.STRING)
+  var status: CustomerStatus,
 )
